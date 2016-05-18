9f519370161a4733943b6ef3e75fd22c3e64b56a
Oren Berkowitz <oberkowitz@pandora.com>
2016-05-18 16:13:41 -0700

    Adding other clock examples and updating compile script:

--- experiment/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcc -Wall server-alsasrc-PCMA.c -o server $(pkg-config --cflags --libs gstreamer-1.0) -lgstnet-1.0
-
-gcc -Wall client-PCMA.c -o client $(pkg-config --cflags --libs gstreamer-1.0) -lgstnet-1.0
